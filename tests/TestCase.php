55f222fa9ab785b48ba29477deaf5532369d8c42
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2023-08-08 14:53:37 +0100

    Some changes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,9 @@
 
 namespace IbrahimBougaoua\FilamentSortOrder\Tests;
 
-use IbrahimBougaoua\FilamentSortOrder\FilamentSortOrderServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'IbrahimBougaoua\\FilamentSortOrder\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            FilamentSortOrderServiceProvider::class,
-        ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-sort-order_table.php.stub';
-        $migration->up();
-        */
-    }
+	
 }
