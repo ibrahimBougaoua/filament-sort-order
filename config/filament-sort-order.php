dd8b715d65c0d9350c68acb23a95322699d29f00
threeel <threeel@users.noreply.github.com>
2024-09-24 00:01:07 +0300

    Clean up

--- config/filament-sort-order.php
+++ config/filament-sort-order.php
@@ -1,12 +1,16 @@
 <?php
 
 // config for IbrahimBougaoua/FilamentSortOrder
-// You can incorporate additional tables by appending "table{number}" as an illustration.
-// 'table1' =>'users',
-// 'table2' =>'posts',
-// 'table3' =>'products',
-//And so on...
 return [
-    'table1' => 'users',
+    
+    /** Add the tables to be migrated */
+    'tables' => [
+        'users'
+    ],
+    
+    /* The column name to be used for sorting */
+    'sort_column_name' => 'sort_column',
+
+    /* Sort Order asc or desc */
     'sort' => 'asc',
 ];
