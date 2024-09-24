a99f80b8d2d59aae1fe491073a0d744b2a14c95d
ibrahimBougaoua <ibrahimBougaoua@users.noreply.github.com>
2024-09-24 10:02:11 +0000

    Fix styling

--- config/filament-sort-order.php
+++ config/filament-sort-order.php
@@ -2,12 +2,12 @@
 
 // config for IbrahimBougaoua/FilamentSortOrder
 return [
-    
+
     /** Add the tables to be migrated */
     'tables' => [
-        'users'
+        'users',
     ],
-    
+
     /* The column name to be used for sorting */
     'sort_column_name' => 'sort_column',
 
